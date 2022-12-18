ada3685c5d5e30645887dbee4d8c0d37a3e02e1c
{kietaro_kanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-18 16:03:35 +0900

    コーデックH264→MP4V

--- datashow.py
+++ datashow.py
@@ -44,7 +44,7 @@ print('画像変換完了')
 #----動画の作成パート----
 def img2mov(dataname):
     outfilename = 'fig/'+''+dataname+'/'+dataname+'.mp4' #作成する動画の名前
-    fourcc = cv2.VideoWriter_fourcc(*'H264') #コーデックの指定
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V') #コーデックの指定
     fps = 5.0 #フレームレート
     width, height = 640, 480 #動画のサイズ
     outfile = cv2.VideoWriter(outfilename, fourcc, fps, (width, height)) #videoweiter
