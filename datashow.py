fa8cac8f9b4054a569c94b15e477aa42cbc0b112
{kietaro_kanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-21 01:02:52 +0900

    読み込み数を250に変更

--- datashow.py
+++ datashow.py
@@ -8,7 +8,7 @@ import numpy as np
 
 #----処理したいデータ---
 data = 'Ex_y050_'
-setting = '_(221219)'
+setting = '_(221220)'
 
 #----グリッドの作成----
 x = np.arange(0, 116, 1)
@@ -23,7 +23,7 @@ def data2fig(x, y, dataname):
     new_dir_path = 'fig/fig'+setting+'/'+analyze_name  #画像を保存するフォルダのパス
     os.makedirs(new_dir_path, exist_ok=True) #画像を保存するフォルダを作成
 
-    for i in range(0, 50):
+    for i in range(0, 250):
         fill0num = f'{i:03}' #数値を0埋めで3桁の文字列にする
         loaddata = np.abs(np.loadtxt('field_/field'+setting+'/'+analyze_name+fill0num+'.txt')) #絶対値でデータ読み込み
         max_value = max(loaddata[2])
@@ -50,7 +50,7 @@ def img2mov(dataname):
     fps = 10.0 #フレームレート
     width, height = 640, 480 #動画のサイズ
     outfile = cv2.VideoWriter(outfilename, fourcc, fps, (width, height)) #videoweiter
-    for i in range(0, 50):
+    for i in range(0, 250):
         fill0num = f'{i:03}'
         read_fig = cv2.imread('fig/fig'+setting+'/'+dataname+'/fig'+fill0num+'.jpg') #画像の読み込み
         outfile.write(read_fig)
