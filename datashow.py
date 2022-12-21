b5d88dd1ba05fd915a1fa63fdcfe053bfd138e70
{keitarokanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-21 17:57:18 +0900

    カラーバー範囲の変更

--- datashow.py
+++ datashow.py
@@ -30,7 +30,7 @@ def data2fig(x, y, dataname):
         max_value = max(loaddata[2])
 
         fig = plt.figure()
-        plt.pcolormesh(x, y, loaddata, cmap='viridis', shading='auto', norm=colors.LogNorm(vmin=1e-3, vmax=5e-1)) #カラーメッシュの作成、カラーバーは対数表示にしている
+        plt.pcolormesh(x, y, loaddata, cmap='viridis', shading='auto', norm=colors.LogNorm(vmin=1e-5, vmax=5e-1)) #カラーメッシュの作成、カラーバーは対数表示にしている
         pp = plt.colorbar(orientation='vertical') #カラーバー
         pp.set_label('Intensity', fontname='Arial', fontsize=18) #カラーバーラベル
 
