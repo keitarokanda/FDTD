7d540306997483fc40cf83149c73fe569b252d83
{keitarokanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-23 12:14:55 +0900

    フレームレート変更

--- datashow.py
+++ datashow.py
@@ -48,7 +48,7 @@ print('画像変換完了')
 def img2mov(dataname):
     outfilename = 'fig/fig'+setting+'/'+dataname+'/'+dataname+'.mp4' #作成する動画の名前
     fourcc = cv2.VideoWriter_fourcc('M','P','4','V') #コーデックの指定
-    fps = 32 #フレームレート
+    fps = 30 #フレームレート
     width, height = 640, 480 #動画のサイズ
     outfile = cv2.VideoWriter(outfilename, fourcc, fps, (width, height)) #videoweiter
     for i in range(0, 512):
