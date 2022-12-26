6ab8f9170fde08366243453cf90af9854ae7e904
{kietaro_kanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-26 23:25:12 +0900

    dipoleの位置変更

--- runfdtd.py
+++ runfdtd.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
 
 
     Dipole = namedtuple('Dipole', ('pol', 'phase', 'x', 'y', 'z'))
-    dipoles= (Dipole('z', 'in', 0, 2.0, 0),) # phase: 'in' in-phase, 'anti' antiphase
+    dipoles= (Dipole('z', 'in', 0, 0, 0),) # phase: 'in' in-phase, 'anti' antiphase
 
 
 # ----field monitor----
