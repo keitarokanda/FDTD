f0e365c2419288972fa33e1fce82c76ab88a8fec
{keitarokanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-27 16:17:38 +0900

    runfdtdのセッティング変更

--- runfdtd.py
+++ runfdtd.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
 
 #----dipoleのセッティング----
     Dipole = namedtuple('Dipole', ('pol', 'phase', 'x', 'y', 'z'))
-    dipoles= (Dipole('z', 'in', 0, 0, 2.0),) # phase: 'in' in-phase, 'anti' antiphase
+    dipoles= (Dipole('z', 'in', 2.0, 0, 0),) # phase: 'in' in-phase, 'anti' antiphase
 
 
 # ----field monitor----
