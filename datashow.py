039c3663b93f77a27040994a3a5d0db50400962d
{kietaro_kanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-29 00:02:01 +0900

    コメント追加

--- datashow.py
+++ datashow.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #----処理したいデータ----
-data_axis = 'Ez_y'
+data_axis = 'Ez_y'  #x_y, x_z, z_y
 data_number = '100_'
 data = str(data_axis+data_number)
 setting = '_test7'
-field = 'epsz_z100'
+field = 'epsz_z100'  #x_y, x_z, z_z
 data_num = 2**9 #データの数
 
 #----グリッドの作成----
