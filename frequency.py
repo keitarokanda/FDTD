bb22cc7a546816fc1b8e553972489ceede4c91e4
{kietaro_kanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2022-12-31 17:43:16 +0900

    図のプロットまで

--- frequency.py
+++ frequency.py
@@ -14,49 +14,52 @@ R_max = 30.0
 P_t = 800 #[W]
 P_min = 1e-12 #[W]
 G_t = 1.64
-f = np.arange(150, 401, 1)
+f = np.arange(150, 401, 0.1)
 
 #光速
 c = 299792458 #[m/s]
-
-
-
 
 #反射係数・透過係数
 reflection = (np.sqrt(epsilon_r1) - np.sqrt(epsilon_r2))**2 / (np.sqrt(epsilon_r1) + np.sqrt(epsilon_r2))**2
 throw = 1-reflection
 
 
+
+#----計算----
 #左辺
-beta = -0.091 * np.sqrt(epsilon_r1) * losstangent *R_max / 5
-y1 = f*10**6/10**(beta*f)
+def y1(x):
+    return x*10**6/10**(-0.091 * np.sqrt(epsilon_r1)*losstangent *R_max*x/5)
 
 #右辺
-y2 = \
-    P_t/P_min * \
-    (G_t**2 * c * RCS)/((4*np.pi)**3 * R_max**4) * \
-    throw**2 * reflection
+def y2(x):
+    return \
+        P_t/P_min * \
+        (G_t**2 * c * RCS)/((4*np.pi)**3 * R_max**4) * \
+        throw**2 * reflection + x*0
+
+def delta(x):
+    return np.abs(y1(x)-y2(x))
 
 
-left_hand = np.array([f, y1])
-right_hand = np.array([f, y2])
+print(min(delta(f)))
 
-print(left_hand)
+#print(right_hand)
 
 #交点の座標を取得
-idx = np.argwhere(np.round(left_hand[1] - right_hand[1]) == 0)
+idx = np.argwhere(np.sign(np.round(y1(f) - y2(f))) == 0)
 
-print(f[idx])
+
+
 #交点をプロット
-plt.plot(f[idx], y1[idx], 'ms', ms=5, label='Intersection', color='green')
+#plt.plot(f[idx], y1(f[idx]), 'ms', ms=5, label='Intersection', color='green')
 
 #交点の座標をグラフに追記
 for i in idx.ravel():
     plt.text(f[i], y1[i], '({x}, {y})'.format(x=f[i], y=y1[i]), fontsize=10)
 
 
-plt.plot(f, y1, color='red', label='left hand')
-plt.axhline(y=y2, xmin=0, xmax=6, )
+plt.plot(f, y1(f), color='red', label='left hand')
+plt.plot(f, y2(f), color='blue', label='right hand' )
 
 
 plt.yscale('log')
